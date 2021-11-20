fe53ebb1e79556c9c8de29f7d27f52777ec9d0b2
shimopino <shimoke4869@gmail.com>
2021-11-20 18:46:45 +0900

    feat: 必要な環境変数と環境変数の検証処理を追加

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,23 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { TaskModule } from './tasks/tasks.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [AuthModule, TaskModule, PrismaModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    AuthModule,
+    TaskModule,
+    PrismaModule,
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().required(),
+        DATABASE_URL: Joi.string().required(),
+        JWT_ACCESS_TOKEN_SECRET: Joi.string().required(),
+        JWT_ACCESS_TOKEN_EXPIRATION_TIME: Joi.number().required(),
+        PORT: Joi.number().required(),
+      }),
+    }),
+  ],
 })
 export class AppModule {}
