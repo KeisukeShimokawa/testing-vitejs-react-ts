3a8eb965f13fd28518e3c251500f77e4ec477cf2
shimopino <shimoke4869@gmail.com>
2021-11-20 12:52:05 +0900

    feat: マイグレーション結果を追加

---
+++ backend/prisma/migrations/20211120035050_init/migration.sql
@@ -0,0 +1,27 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "credentials" (
+    "userId" TEXT NOT NULL,
+    "refreshToken" TEXT NOT NULL,
+    CONSTRAINT "credentials_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "tasks" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "expires" DATETIME NOT NULL,
+    "userId" TEXT NOT NULL,
+    CONSTRAINT "tasks_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "credentials_userId_key" ON "credentials"("userId");
